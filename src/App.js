00d8c38ce494ed125787a906bb4c7aaa6b463f15
Your Name <kumarprince08081999@gmail.com>
2025-03-01 00:39:16 +0530

    newmessage

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h2>Hello World</h2>
-      <h3>Hello World</h3>
-      <h4>Hello World</h4>
+      <h1>I am Prince</h1>
     </div>
   );
 }
